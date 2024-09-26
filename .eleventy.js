c45993da338ac6cd68b286ea49aef635306ee191
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-09-26 02:57:33 +0000

    Markdown parsing and a bit more.

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,17 @@
+const markdownIt = require("markdown-it");
+
+const md = new markdownIt();
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setDataDeepMerge(true);
-  eleventyConfig.addGlobalData("eleventyComputed", {
-    debug: function () {
-      return this;
-    },
+  eleventyConfig.addFilter("markdown", function (content) {
+    if (typeof content === "string") {
+      console.log("Rendering", content);
+      return md.render(content);
+    } else {
+      return content;
+    }
   });
+
   return {
     dir: {
       input: "src",
--- src/_data/projects.js
+++ src/_data/projects.js
@@ -3,12 +3,28 @@ module.exports = [
     slug: "search",
     title: "Personality Quiz",
     assignment: "Binary Search Project",
-    authors: ["Tom H", "Mickey M."],
+    authors: ["Joe S", "Mickey M."],
+    markdown: `
+## Project
+In this project we worked really hard on binary search.
+
+## Group
+My group was amazing!
+
+- Example
+- List
+- in Markdown
+    `,
   },
   {
     slug: "portfolio",
     title: "Tom's Portfolio",
     assignment: "APCSP Portfolio",
     authors: ["Tom"],
+    html: `
+    <p>Hello world</p>
+    <p><b>Example</b> paragraph</p>
+    `,
+
   },
 ];
