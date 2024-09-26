78106c3da36d6bf3c83dc267e874d76d97d93523
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-09-26 11:29:36 +0000

    Update to remove data structure and focus instead on component architecture -- one simple concept here.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   vite: {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,1 @@
-import './style.css';
-console.log("Hello World!");
+console.log("Hello World! Confirming we imported JavaScript correctly.");
